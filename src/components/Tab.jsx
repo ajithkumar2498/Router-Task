6a9e628eaa9bdcda18a998d83c7d476c6321e17e
ajithkumar2498 <ajibhc24@gmail.com>
2023-12-18 22:58:18 +0530

    hii

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -11,16 +11,16 @@ function Tab() {
   <div className="HeaderTab">
   <Nav variant="underline" defaultActiveKey="*">
       <Nav.Item>
-        <Link className="Card-link" to= "/all"><b>ALL</b></Link>
+        <Link className="Card-link" to= "/all" tabIndex= "1"><b>ALL</b></Link>
       </Nav.Item>
       <Nav.Item>
-        <Link className="Card-link" to= "/fsd"> <b>Full Stack Development</b></Link>
+        <Link className="Card-link" to= "/fsd" tabIndex= "1" > <b>Full Stack Development</b></Link>
       </Nav.Item>
       <Nav.Item>
-      <Link className="Card-link" to= "/cs"> <b>Cyber Security</b></Link>
+      <Link className="Card-link" to= "/cs" tabIndex= "1" > <b>Cyber Security</b></Link>
       </Nav.Item>
       <Nav.Item>
-      <Link className="Card-link" to= "/ds"><b>Data Science</b></Link>
+      <Link className="Card-link" to= "/ds" tabIndex= "1" ><b>Data Science</b></Link>
       </Nav.Item>
     </Nav>
     </div>
