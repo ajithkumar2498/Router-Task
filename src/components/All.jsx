745f54891a60162a1e7cde66e4328930b13f1926
ajithkumar2498 <ajibhc24@gmail.com>
2023-12-19 15:27:36 +0530

    new

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -5,17 +5,18 @@ import Card from 'react-bootstrap/Card';
 function All({image, content, heading}) {
     
   return <>
-  
-  <div className="row">
-   <Card className="cards" style={{ width: '20rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>{heading}</Card.Title>
-        <Card.Text>
-          {content}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+  <div className="container">
+      <div className="row">
+          <Card className="cards" style={{ width: '20rem' }}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>{heading}</Card.Title>
+                <Card.Text>
+                  {content}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+        </div>
     </div>
   </>
 }
